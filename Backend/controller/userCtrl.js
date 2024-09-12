e367a6e8e3ff12d04ffc72428959bca0bef9cbe4
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-09-12 09:57:41 -0500

    change webHook URL

--- Backend/controller/userCtrl.js
+++ Backend/controller/userCtrl.js
@@ -763,12 +763,12 @@ module.exports = {
     }
   },
 
-  // Google Sheet Webhook URL
+  // Google Sheet Webhook 
 
   addSheetItem: async (req, res) => {
     // Google Sheet Webhook URL
     const googleSheetWebhookURL =
-      "https://script.google.com/macros/s/AKfycbxQSe8f6i3ifmgC7oepM72UQC90gLkehtnjrvYFFzbHAzw_MPk-5dwFBuhqsh69JfjOGg/exec?gid=0";
+     "https://script.google.com/macros/s/AKfycbxQSe8f6i3ifmgC7oepM72UQC90gLkehtnjrvYFFzbHAzw_MPk-5dwFBuhqsh69JfjOGg/exec?gid=0";
 
     // Multer configuration for image uploads
     const storage = multer.diskStorage({

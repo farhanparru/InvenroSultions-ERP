d1f33737d8570adbd7067070810cc62c48baa3b4
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2024-09-24 06:34:36 -0500

    server up

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const userRouter = require('./router/userRouter')
 const {Server} = require('ws');
 
 app.use(cors({
-    origin:"https://invenro.com",
+    origin:"https://www.invenro.com",
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
